04218f8465f229118cb2eb712c1bef664c834398
Corjen <corjen.moll@gmail.com>
2021-02-18 17:27:38 +0100

    Init

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fetch = require('node-fetch')
+try {
+  // `who-to-greet` input defined in action metadata file
+
+  const vercelToken = core.getInput('vercel_token')
+  const teamId = core.getInput('team_id')
+  const projectId = core.getInput('project_id')
+  const githubCommitRef = core.getInput('github_commit_ref')
+  console.log(`teamId ${teamId}!`);
+  console.log(`projectId ${projectId}!`);
+  console.log(`githubCommitRef ${githubCommitRef}!`);
+  console.log(`url https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`)
+  const response = fetch(`https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`, {headers: {
+    authorization: `Bearer ${vercelToken}`
+  }}).then(res => res.json())
+
+  const filtered = response.deployments.filter(d => d.meta.githubCommitRef === githubCommitRef)
+  console.log(filtered)
+  if(filtered.length === 0) {
+    throw new Error('No deployments found')
+  }
+
+
+  core.setOutput("url", filtered[0].url);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}

deb0e3be935a543741609dc90e99244d95011ea7
Corjen <corjen.moll@gmail.com>
2021-02-22 09:46:49 +0100

    Possible fix for empty url

--- dist/index.js
+++ dist/index.js
@@ -8,20 +8,29 @@ module.exports =
 const core = __nccwpck_require__(699);
 const github = __nccwpck_require__(806);
 const fetch = __nccwpck_require__(486)
-try {
-  const vercelToken = core.getInput('vercel_token')
-  const teamId = core.getInput('team_id')
-  const projectId = core.getInput('project_id')
-  const githubCommitRef = core.getInput('github_commit_ref')
+const wait = __nccwpck_require__(368)
 
-  fetch(`https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`, {headers: {
-    authorization: `Bearer ${vercelToken}`
-  }}).then(res => res.json()).then(response => {
+async function run () {
+  try {
+    const vercelToken = core.getInput('vercel_token')
+    const teamId = core.getInput('team_id')
+    const projectId = core.getInput('project_id')
+    const githubCommitRef = core.getInput('github_commit_ref')
+
+    await wait(5000)
+    const response = fetch(`https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`, {headers: {
+      authorization: `Bearer ${vercelToken}`
+    }}).then(res.json)
+
+    core.info(`Input`, {
+      projectId,
+      githubCommitRef
+    })
 
     const filtered = response.deployments.filter(d => d.meta.githubCommitRef === githubCommitRef)
 
     if(filtered.length === 0) {
-      throw new Error('No deployments found')
+      throw new Error(`No deployments found`)
     }
 
     console.log(`url: ${filtered[0].url}`)
@@ -30,12 +39,13 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-  })
 
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch')
-try {
-  const vercelToken = core.getInput('vercel_token')
-  const teamId = core.getInput('team_id')
-  const projectId = core.getInput('project_id')
-  const githubCommitRef = core.getInput('github_commit_ref')
+const wait = require('./')
 
-  fetch(`https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`, {headers: {
-    authorization: `Bearer ${vercelToken}`
-  }}).then(res => res.json()).then(response => {
+async function run () {
+  try {
+    const vercelToken = core.getInput('vercel_token')
+    const teamId = core.getInput('team_id')
+    const projectId = core.getInput('project_id')
+    const githubCommitRef = core.getInput('github_commit_ref')
+
+    await wait(5000)
+    const response = fetch(`https://api.zeit.co/v5/now/deployments?teamId=${teamId}&projectId=${projectId}`, {headers: {
+      authorization: `Bearer ${vercelToken}`
+    }}).then(res.json)
+
+    core.info(`Input`, {
+      projectId,
+      githubCommitRef
+    })
 
     const filtered = response.deployments.filter(d => d.meta.githubCommitRef === githubCommitRef)
 
     if(filtered.length === 0) {
-      throw new Error('No deployments found')
+      throw new Error(`No deployments found`)
     }
 
     console.log(`url: ${filtered[0].url}`)
@@ -23,9 +32,10 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-  })
 
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+run()
